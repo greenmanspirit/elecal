a054fe6b2debb986d7939d6ab38dcaa336a6072e
Adam Hobaugh <adam@cs.pitt.edu>
2017-02-01 17:23:43 -0500

    initial project files

---
+++ main.js
@@ -0,0 +1,17 @@
+const {BrowserWindow, app} = require('electron');
+
+app.on('ready', () => {
+    let appWindow = new BrowserWindow({show: false});
+    appWindow.once('ready-to-show', () => {
+        appWindow.show();
+    })
+    appWindow.loadURL('file://' + __dirname + '/index.html');
+    appWindow.on('closed', function() {
+        mainWindow = null;
+    });
+})
+app.on('window-all-closed', function() {
+    if (process.platform != 'darwin') {
+        app.quit();
+    }
+});
