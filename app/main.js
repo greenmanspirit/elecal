0254a80af36254ef85636b3d488e83ea46c304a5
Adam Hobaugh <adam@cs.pitt.edu>
2017-02-02 16:17:01 -0500

    added semicolons

--- app/main.js
+++ app/main.js
@@ -4,12 +4,12 @@ app.on('ready', () => {
     let appWindow = new BrowserWindow({show: false});
     appWindow.once('ready-to-show', () => {
         appWindow.show();
-    })
+    });
     appWindow.loadURL('file://' + __dirname + '/index.html');
     appWindow.on('closed', function() {
         mainWindow = null;
     });
-})
+});
 app.on('window-all-closed', function() {
     if (process.platform != 'darwin') {
         app.quit();
