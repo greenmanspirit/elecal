96960cd35337d9ed0ab7bbd7c4887340d922aa0e
Adam Hobaugh <adam@hobaugh.us>
2017-02-05 11:32:03 -0500

    process.env is not available in the renderer process so I added an ipc function so I can get the config directory in renderer.

--- app/main.js
+++ app/main.js
@@ -1,5 +1,7 @@
-const {BrowserWindow, app} = require('electron');
+const {BrowserWindow, app, ipcMain} = require('electron');
+const path = require('path');
 
+// Get the window up and running
 app.on('ready', function() {
     let appWindow = new BrowserWindow({backgroundColor: '#fafafa'});
     appWindow.loadURL('file://' + __dirname + '/index.html');
@@ -11,4 +13,9 @@ app.on('window-all-closed', function() {
     if (process.platform != 'darwin') {
         app.quit();
     }
+});
+
+let config_path = path.join(process.env.HOME, '.config', 'elecal');
+ipcMain.on('getConfigDir', (event, arg) => {
+  event.returnValue = config_path;
 });
