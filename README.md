7eed6e96d28fee2b77a3b527557b506e1c346dd6
Adam Hobaugh <adam@cs.pitt.edu>
2017-02-17 16:15:56 -0500

    Added base tests and needed modules. Updated README in conjuction

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
 # Elecal
 
-[![Node.js Version](https://img.shields.io/badge/Node.js-v4.2.6-blue.svg)](http://nodejs.org/)
+[![Node.js Version](https://img.shields.io/badge/node.js-v4.2.6-blue.svg)](http://nodejs.org/)
 [![npm Version](https://img.shields.io/badge/npm-v3.5.2-blue.svg)](https://www.npmjs.com/)
-[![Electron Version](https://img.shields.io/badge/Electron-v1.4.15-blue.svg)](http://electron.atom.io/)
+[![Electron Version](https://img.shields.io/badge/electron-v1.4.15-blue.svg)](http://electron.atom.io/)
 
 
-[![Standard - JavaScript Style Guide](https://img.shields.io/badge/JS%20Code%20Style-Standard-brightgreen.svg)](http://standardjs.com/)
-[![CSS Lint - CSS Style Guide](https://img.shields.io/badge/CSS%20Code%20Style-CSS%20Lint-brightgreen.svg)](http://csslint.net/)
+[![Standard - JavaScript Style Guide](https://img.shields.io/badge/js%20code%20style-standard-brightgreen.svg)](http://standardjs.com/)
+[![CSS Lint - CSS Style Guide](https://img.shields.io/badge/css%20code%20style-css%20lint-brightgreen.svg)](http://csslint.net/)
+![Tests](https://img.shields.io/badge/tests-passing-brightgreen.svg)
 
-Elecal will be **Ele**ctron **Cal**endar Management program.
+Elecal will be the **Ele**ctron **Cal**endar Management program.
 
 Every calendar app I have tried is almost where I want it to be feature wise. However, I can't find an app that has every feature I want. As a result, I decided to try and make my own.
---
+++ test/test.js
@@ -0,0 +1,62 @@
+/*
+ * Tests for Elecal using Mocha, Chai, ChaiAsPromised and Spectron
+ */
+
+/*
+ * Imports
+ */
+// This is not necessary for mocha to work but is needed for standardjs to pass
+const describe = require('mocha').describe
+const it = require('mocha').it
+const beforeEach = require('mocha').beforeEach
+const afterEach = require('mocha').afterEach
+
+// Actually needed imports for testing
+const Application = require('spectron').Application
+const path = require('path')
+const chai = require('chai')
+const chaiAsPromised = require('chai-as-promised')
+
+/*
+ * Global variables
+ */
+// Paths needed for the Application object
+const appPath = path.resolve(__dirname, '../app/main.js')
+const electronPath = path.resolve(__dirname, '../node_modules/.bin/electron')
+
+// Configure chai before every test
+global.before(function () {
+  chai.should()
+  chai.use(chaiAsPromised)
+  this.app = new Application({
+    path: electronPath,
+    args: [appPath]
+  })
+})
+
+/*
+ * Tests
+ */
+describe('application launch', function () {
+  this.timeout(10000)
+
+  beforeEach(function () {
+    return this.app.start()
+  })
+
+  afterEach(function () {
+    if (this.app && this.app.isRunning()) {
+      return this.app.stop()
+    }
+  })
+
+  it('shows an initial window', function () {
+    return this.app.client.waitUntilWindowLoaded()
+      .getWindowCount().should.eventually.equal(1)
+  })
+
+  it('shows the right title', function () {
+    return this.app.client.waitUntilWindowLoaded()
+      .getTitle().should.eventually.equal('Elecal')
+  })
+})
